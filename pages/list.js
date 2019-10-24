d8a183a5b8e81f671873ffb6b157dad31336e9cf
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-10-25 01:04:37 +0800

    add layout

---
+++ pages/list.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const List = () => (
+  <div>
+    
+    <h1>list</h1>
+  </div>
+)
+
+export default List
