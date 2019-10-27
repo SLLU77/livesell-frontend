05e203f13dff50dffa24e9e49a225791fbbd618d
Tsai Mujing <moojitsai@gmail.com>
2019-10-28 07:47:25 +0800

    add Swiper

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,18 +1,35 @@
 import React from 'react'
- 
+import styles from './Header.scss'
 
 // import Link from 'next/link'
 // import Head from 'next/head'
 // import Nav from '../components/nav'
 
-const Header = () => (
+const Header = ({   disableBackBtn = false, disableCartBtn = false, handleBackClick }) => {
+    
+    const handleClick = e => {
+        if (handleBackClick) {
+            handleBackClick()
+        } else {
+            window.history.back()
+        }
+    }
+    
+    return (
     
     
-        <> 
-            Header
-        </>
+        <div className={styles.header} > 
+            {!disableBackBtn && 
+            <div className={styles.iconBack}
+                    onClick={handleClick} 
+                    data-testid="backBtn" /> }
+        
+        
+
+        </div>
     
-)
+    ) 
+}
 
 // List.getInitialProps =   context => {
 //     //https://liveserverpy.herokuapp.com/api/v1/products/
--- pages/list.js
+++ pages/list.js
@@ -1,18 +1,74 @@
 import React from 'react'
+import Swiper from 'react-id-swiper';
+import 'isomorphic-fetch'
 import Layout from '@components/Layout/Layout'
+import styles from './list.scss'
 
-// import Link from 'next/link'
-// import Head from 'next/head'
-// import Nav from '../components/nav'
 
-const List = () => (
-    <Layout disableBackBtn={true}>
+ 
+
+const List = ({ productList = [], ...props }) => {
+    console.log('productList',productList);
+    (<Layout disableBackBtn={true}>
+        <Swiper
+           pagination={{
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+        }}>
+              
+            <div>
+                <img src="/static/images/banner-01.jpg" />
+            </div>
+            <div>
+                <img src="/static/images/banner-02.jpg" />
+            </div>
+            <div>
+                <img src="/static/images/banner-03.jpg" />
+            </div>
     
+        </Swiper>
         <h1>list</h1>
     </Layout>
 )
+    return (
+        (
+            <Layout disableBackBtn={true}>
+                <Swiper
+                   pagination={{
+                    el: '.swiper-pagination',
+                    clickable: true,
+                    dynamicBullets: true
+                }}>
+                      
+                    <div>
+                        <img src="/static/images/banner-01.jpg" />
+                    </div>
+                    <div>
+                        <img src="/static/images/banner-02.jpg" />
+                    </div>
+                    <div>
+                        <img src="/static/images/banner-03.jpg" />
+                    </div>
+            
+                </Swiper>
+                {/* <ul className={styles.categoryList}>
+                    {productList.products.map((item, i) => (
+                        <li onClick={e => setCateIndex(i)} key={item.cateId}
+                            >
+                            {item.category}
+                        </li>
+                    ))}
+                </ul> */}
+                <h1>list</h1>
+            </Layout>
+        )
+    )
+}
 
-// List.getInitialProps =   context => {
-//     //https://liveserverpy.herokuapp.com/api/v1/products/
-// }
+   List.getInitialProps = async  context => {
+    const res = await fetch('https://flask-shopping.herokuapp.com/api/v1/product')
+    const json = await res.json()
+    return { productList: json }
+}
 export default List
