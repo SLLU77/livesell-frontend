c8cbc6d1b238564ead12e13aca645b9948fc8d7a
Neil <Neil.Lu@qct.io>
2019-10-29 19:18:50 +0800

    add components for cart.js
    components/CartProductItem/
    components/CartStep/
    components/Counter/
    components/Form/
    components/Image/
    components/Modal/
    components/Portal/
    hocs/
    styles/modules/_sectionBox.scss
    utils/

---
+++ components/Image/Image.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './Image.scss'
+class Image extends React.Component {
+    constructor(props) {
+        super(props)
+        this.imageRef = React.createRef()
+    }
+    componentDidMount() {
+        const observer = new IntersectionObserver(
+            entries => {
+                let entry = entries[0]
+                if (entry.intersectionRatio <= 0) return
+                entry.target.style.backgroundImage = `url("${entry.target.dataset.src}"`
+                observer.unobserve(entry.target)
+            },
+            {
+                rootMargin: '30px'
+            }
+        )
+        observer.observe(this.imageRef.current)
+    }
+    render() {
+        return (
+            <div className={`${this.props.className} ${styles.image}`} data-src={this.props.src} ref={this.imageRef} />
+        )
+    }
+}
+export default Image
